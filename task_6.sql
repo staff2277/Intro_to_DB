32e7a73d521f456013b0caea0f3d6360210ebeb6
Mustapha Osman <wnfynn@gmail.com>
2025-02-01 22:13:06 +0000

    q6

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,9 @@
-USE alx_book_store;
-
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES 
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+VALUES
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+
+
+-- Optional: Check if the data was inserted correctly
+SELECT * FROM customer WHERE customer_id IN (2, 3, 4); 
